8370a6fc26c617fd832279a9b50a1f6636744426
Kundan Paudel <181020254+kundanpdl@users.noreply.github.com>
2025-05-10 17:27:35 -0600

    Scripts added for prod, removed node modules folders

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import path from "path";
 
 const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Serve static files from the React frontend app
+const __dirname = path.resolve();
+
 dotenv.config();
 
 // Middleware to parse JSON data from incoming requests from frontend or Postman
@@ -12,7 +18,12 @@ app.use(express.json());
 // Using productRoutes from the productRoutes.js file to handle all the requests
 app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 8000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
